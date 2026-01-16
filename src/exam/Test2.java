3d1060a49ba7487f249c6e6e50af61d1ed94cd5e
drynluv <drynluv@gmail.com>
2026-01-16 16:26:29 +0900

    20260116_1625 그림 전송 유무

---
+++ src/exam/Test2.java
@@ -0,0 +1,16 @@
+package exam;
+
+public class Test2 {
+	public static void main(String[] args) {
+		int tot = 0;
+		
+		for(int i=1; i<=100; i++) {
+			tot += i;
+			if(i % 10 == 0) {
+				
+			System.out.println("1부터 "+i+"까지의 합은 "+tot+"입니다.");
+		}
+		}
+	}
+}
+
